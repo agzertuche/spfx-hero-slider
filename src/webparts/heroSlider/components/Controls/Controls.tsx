791559471b5fbf0919890a8c1aca6b8f53947c76
Arturo De la Garza <agzertuche@gmail.com>
2019-05-13 23:06:18 -0500

    session one

---
+++ src/webparts/heroSlider/components/Controls/Controls.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { ControlsProps } from './ControlsProps';
+import styles from './Controls.module.scss';
+
+const Controls: React.StatelessComponent<ControlsProps> = ({
+  goNext,
+  goPrevious,
+}) => {
+  return (
+    <nav className={styles.controls}>
+      <button type="button" className={styles.arrowLeft} onClick={goPrevious} />
+      <button type="button" className={styles.arrowRight} onClick={goNext} />
+    </nav>
+  );
+};
+
+export default Controls;
